1ba9a88536af497971cfeedcc591fb0d0caeb7d6
Marcio de Andrade <marcio@requestdev.net>
2024-12-15 07:32:41 -0400

    Criação da pasta Models e da classe UserRegistrationRequest

---
+++ WebApiFluentValidation/Models/UserRegistrationRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiFluentValidation.Models
+{
+    public class UserRegistrationRequest
+    {
+        public string? Nome { get; set; }
+        public string? SobreNome { get; set; }
+        public string? Email { get; set; }
+        public string? Senha { get; set; }
+        public string? ConfirmaSenha { get; set; }
+    }
+}
