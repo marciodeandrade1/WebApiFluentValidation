864c7fef0c21f9b479c98ea5c52888ea40e3073f
Marcio de Andrade <marcio@requestdev.net>
2024-12-15 07:46:32 -0400

    Criação da pasta Validations e da classe UserRegistrationValidator

---
+++ WebApiFluentValidation/Validations/UserRegistrationValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using WebApiFluentValidation.Models;
+
+namespace WebApiFluentValidation.Validations
+{
+    public class UserRegistrationValidator : AbstractValidator<UserRegistrationRequest>
+    {
+        public UserRegistrationValidator()
+        {
+            RuleFor(x => x.Nome).NotEmpty().MinimumLength(4);
+            RuleFor(x => x.SobreNome).NotEmpty().MaximumLength(10);
+            RuleFor(x => x.Email).EmailAddress().WithMessage("{PropertyName} inválida! Favor verificar!");
+            RuleFor(x => x.Senha).Equal(z => z.ConfirmaSenha).WithMessage("Senhas não são iguais!");
+        }
+    }
+}
